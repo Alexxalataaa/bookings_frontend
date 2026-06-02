b52d58a57b17949a78ff856103db6fabb7dd025b
luisnzx <luisnugent3@gmail.com>
2026-06-02 10:05:57 +0200

    Delete bookings_frontend-develop directory

--- bookings_frontend-develop/app/(admin)/customers/page.tsx
+++
@@ -1,8 +0,0 @@
-import CustomersClient from "./CustomersClient";
-import { getCustomers } from "@/lib/api";
-
-export default async function CustomersPage() {
-  const customers = await getCustomers();
-
-  return <CustomersClient initialCustomers={customers} />;
-}
--- bookings_frontend-develop/app/(admin)/payments/page.tsx
+++
@@ -1,8 +0,0 @@
-import PaymentsClient from "./PaymentsClient";
-import { getPayments } from "@/lib/api";
-
-export default async function PaymentsPage() {
-  const payments = await getPayments();
-
-  return <PaymentsClient initialPayments={payments} />;
-}
